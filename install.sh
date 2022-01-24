931bacb36d16042e09101ff721829ab580a350a4
Nick Butler <nick@codeindulgence.com>
2022-01-24 17:00:44 +1100

    Fix for install script

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
+echo Running script: $0
+
+function try() {
+  for i in $(seq 10); do
+    $* && break
+    sleep 10
+  done
+}
+
+set -x
+
 # Get our code
 token=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
 url=https://codeload.github.com/EY-Data-Science-Program/2022-Better-Working-World-Data-Challenge/zip/main
-try wget --header "Authorization:  token $token" $url -O /tmp/archive.zip
+try curl github.com
+wget --header "Authorization:  token $token" $url -O /tmp/archive.zip
 unzip /tmp/archive.zip
 codepath=/home/frog/challenge
 mv 2022-Better-Working-World-Data-Challenge-main $codepath
