ab1ae4111af547c4f96342cf44cf92e4f416fd9d
Nick Butler <nick@codeindulgence.com>
2022-01-27 11:54:02 +1100

    Update install script to extract data

--- platform/install.sh
+++ platform/install.sh
@@ -15,6 +15,9 @@ url=https://codeload.github.com/EY-Data-Science-Program/2022-Better-Working-Worl
 token=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
 wget --header "Authorization:  token $token" $url -O /tmp/archive.zip
 unzip /tmp/archive.zip
+cd 2022-Better-Working-World-Data-Challenge-main/data
+tar xvjf GBIF_Occurence_Data_v1.tar.bz
+cd ../..
 codepath=/home/frog/challenge
 mkdir -p $codepath
 mv 2022-Better-Working-World-Data-Challenge-main/notebooks $codepath/
