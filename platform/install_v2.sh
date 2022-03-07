81da7e6bdf3a7246683f04eeeb058ce2516ba308
tecklowdna <51142868+tecklowdna@users.noreply.github.com>
2022-03-07 12:54:48 +1100

    Update and rename install_old.sh to install_v2.sh

--- platform/install_v2.sh
+++ platform/install_v2.sh
@@ -12,14 +12,15 @@ set -x
 # Get our code
 try curl github.com
 url=https://codeload.github.com/EY-Data-Science-Program/2022-Better-Working-World-Data-Challenge/zip/main
-token=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
-wget --header "Authorization:  token $token" $url -O /tmp/archive.zip
+# token=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
+# wget --header "Authorization:  token $token" $url -O /tmp/archive.zip
+wget $url -O /tmp/archive.zip
 unzip /tmp/archive.zip
-cd 2022-Better-Working-World-Data-Challenge-main/data
-tar xvjf GBIF_Occurence_Data_v1.tar.bz
+# cd 2022-Better-Working-World-Data-Challenge-main/data
+# tar xvjf GBIF_Occurence_Data_v1.tar.bz
 cd ../..
 codepath=/home/notebooks/frog/challenge
 mkdir -p $codepath
 mv 2022-Better-Working-World-Data-Challenge-main/notebooks $codepath/
-mv 2022-Better-Working-World-Data-Challenge-main/data $codepath/
+# mv 2022-Better-Working-World-Data-Challenge-main/data $codepath/
 chown -R frog:frog $codepath
