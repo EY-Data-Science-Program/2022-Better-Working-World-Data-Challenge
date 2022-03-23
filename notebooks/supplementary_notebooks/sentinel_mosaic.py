164b8f8fb97543fe3e8b80a597537bdab6624aca
tecklowdna <51142868+tecklowdna@users.noreply.github.com>
2022-03-24 09:36:08 +1100

    Update sentinel_mosaic.py

--- notebooks/supplementary_notebooks/sentinel_mosaic.py
+++ notebooks/supplementary_notebooks/sentinel_mosaic.py
@@ -1,18 +1,30 @@
+import os
+from glob import escape
 from azure.storage.blob import BlobServiceClient
 import xarray as xr
 import re
+import sys
 
 # Helper functions
+
+local_path = "./data"
+
+if not os.path.exists(local_path):
+    os.makedirs(local_path)
+
+
 def parse_coords(coords):
     lat, lat_dec, lon, lon_dec = coords.split('_')
     lat = re.sub('^[A-Z]*0*', '', lat)
     lon = re.sub('^[A-Z]*0*', '', lon)
     return eval(f"(-{lon}.{lon_dec}, {lat}.{lat_dec}, -{lon}.{lon_dec}+0.25, {lat}.{lat_dec}+0.25)")
 
+
 def bbox_intersects(box1, box2):
     left1, bottom1, right1, top1 = box1
     left2, bottom2, right2, top2 = box2
     return not(left2 >= right1 or right2 <= left1 or top2 <= bottom1 or bottom2 >= top1)
+
 
 def get_scenes(blobs, bbox):
     target_scenes = []
@@ -26,33 +38,46 @@ def get_scenes(blobs, bbox):
 
 def get_mosaic(bbox):
     # Retrieve the blobs
-    cred='sv=2020-08-04&ss=b&srt=sco&sp=rl&se=2022-03-31T11:20:06Z&st=2022-03-02T03:20:06Z&spr=https,http&sig=dq4v74MkYeyrMprEJtunRTgZhjfnAwW7Q6Dr%2BfLbmbQ%3D' # Shared Access Signature
-    url="https://storagepsok6jlzdzmrk.blob.core.windows.net"
-    container='datachallenge2022'
+    cred = 'sv=2020-10-02&st=2022-03-21T10%3A09%3A29Z&se=2022-06-30T09%3A09%3A00Z&sr=c&sp=rl&sig=VdoMJdqis89RWVSYRscPFwNujKoXXQx6dteC%2FUVlV6Y%3D'  # Shared Access Signature
+    url = "https://euwpbww002sta01.blob.core.windows.net"
+    container = 'mosaicdatachallenge2022'
     blob_service_client = BlobServiceClient(account_url=url, credential=cred)
-    container_client = blob_service_client.get_container_client(container='datachallenge2022')
+    container_client = blob_service_client.get_container_client(
+        container=container)
     blobs = {}
     for blob in container_client.list_blobs():
-        region, coords, _ = blob.name.split('/')
-        if region not in blobs.keys():
-            blobs[region] = {(f'{region}/{coords}', parse_coords(coords))}
-        else:
-            blobs[region].add((f'{region}/{coords}', parse_coords(coords)))
-            
-    
+        try:
+            region, coords, _ = blob.name.split('/')
+            if region not in blobs.keys():
+                blobs[region] = {(f'{region}/{coords}', parse_coords(coords))}
+            else:
+                blobs[region].add((f'{region}/{coords}', parse_coords(coords)))
+        except Exception as e:
+            print(e)
+
     # Get scenes
     scenes = get_scenes(blobs, bbox)
     print(f"{len(scenes)} scenes intersect")
     if len(scenes) == 0:
         print('No scenes found')
         return
-    
+
     # download scenes
     all_data = None
     for scene in scenes:
         data = None
         for tif in ['red_median', 'green_median', 'blue_median', 'nir_median']:
-            tif_data = xr.open_rasterio(f'{url}/{container}/{scene}/{tif}.tif').assign_coords(band=[tif.split('_')[0]])
+            scene_path = os.path.join(local_path, scene)
+            if not os.path.exists(scene_path):
+                os.makedirs(scene_path)
+            blob_client = blob_service_client.get_blob_client(
+                container=container, blob=f'{scene}/{tif}.tif')
+            download_file_path = os.path.join(scene_path, f'{tif}.tif')
+            print("\nDownloading blob to \n\t" + download_file_path)
+            with open(download_file_path, "wb") as download_file:
+                download_file.write(blob_client.download_blob().readall())
+            tif_data = xr.open_rasterio(download_file_path).assign_coords(
+                band=[tif.split('_')[0]])
             if data is None:
                 data = tif_data
             else:
@@ -61,11 +86,12 @@ def get_mosaic(bbox):
         if all_data is None:
             all_data = data.rename('var')
         else:
-            all_data = all_data.combine_first(data)   
-    
+            all_data = all_data.combine_first(data)
+
     # Crop to bounding box
-    all_data = all_data.where((all_data.x >= bbox[0]) &\
-                              (all_data.x <= bbox[2]) &\
-                              (all_data.y >= bbox[1]) &\
+    all_data = all_data.where((all_data.x >= bbox[0]) &
+                              (all_data.x <= bbox[2]) &
+                              (all_data.y >= bbox[1]) &
                               (all_data.y <= bbox[3]), drop=True)
+
     return all_data
